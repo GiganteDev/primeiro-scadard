8d821f63af6f1aa5dadaac1ac39a062fcdb7e981
Pedro Gabriel Gigante <q1dcc21@gmail.com>
2019-09-01 19:42:30 -0300

    Update dashboard1.Designer.cs

--- dashboard1.Designer.cs
+++ dashboard1.Designer.cs
@@ -34,8 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(251)))));
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(43)))), ((int)(((byte)(42)))));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "dashboard1";
             this.Size = new System.Drawing.Size(877, 562);
