9d9f85717d0f1d356e14e0ef5e1f88c988c50972
Pedro Gabriel Gigante <q1dcc21@gmail.com>
2019-09-01 19:43:06 -0300

    Create NovaClasse.cs

---
+++ NovaClasse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADARD
+{
+    class NovaClasse
+    {
+        private static string _chtxt;
+        public static string chtxt
+        {
+            get { return _chtxt; }
+            set { _chtxt = value; }
+        }
+        private static int _mudeacor;
+        public static int mudeacor
+        {
+            get { return _mudeacor; }
+            set { _mudeacor = value; }
+        }
+    }
+}
